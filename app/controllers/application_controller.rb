09b5124864274330bb2e3f2d5e3f5b184a228bc5
Jimmy <jimmlindsey@gmail.com>
2023-11-04 13:37:26 +0000

    add username to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def configure_permitted_parameters
+    extra_keys = [:username]
+    signup_keys = extra_keys
+    devise_parameter_sanitizer.permit(:sign_up, keys: signup_keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: extra_keys)
+  end
 end
